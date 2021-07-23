9cf8c32fb9e8d9890423f2a655d80138dea5de1d
David Fachini <david.fachini@owshq.com>
2021-07-23 12:31:46 -0300

    segundo commit

---
+++ PythonExercicios/ex002.py
@@ -0,0 +1,3 @@
+nome = input('Qual o seu nome? ')
+# print('Olá ' + nome +' ! Prazer em te conhecer!')
+print('Olá {}!. Prazer te conhecer.'.format(nome))
---
+++ PythonExercicios/ex003.py
@@ -0,0 +1,5 @@
+num1 = int(input('Digite um numero: '))
+num2 = int(input('Digite um numero: '))
+soma = num1 + num2
+#print('A soma dos 2 numeros é :', soma)
+print('A soma dos 2 numeros é {}: '.format(soma))
